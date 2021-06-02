527ba72c8fc13ab6d8a04ccab75e0ddcd2500ed3
HebaRamadan <81022693+HebaRamadan@users.noreply.github.com>
2021-06-02 16:25:22 +0200

    Update UserInterface.h

--- Clinic_System_Using_LinkedList/UserInterface.h
+++ Clinic_System_Using_LinkedList/UserInterface.h
@@ -6,9 +6,9 @@
 #define   USER_INTERFACE_H_
 
 /* for admin mode*/
-u8    	CheckPassword			  ( void			  	);                //done
+u8    	CheckPassword			 ( void			  	);                //done
 Node *	AddPatientRecord	  ( Node * Start	);      		      //done
-void	EditInformationPatient ( Node * Start	);     		        //done
+void	EditInformationPatient  ( Node * Start	);     		        //done
 void	DisplayAvaliableSlots	( Node * Start	);                //done
 void	ReserveSlotPatient	( Node * Start	);                //done
 Node *	CancelReseravation ( Node * Start	);                //done
